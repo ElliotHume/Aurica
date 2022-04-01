8d4fac42b4eee019758f58b9887a5a507899c3e4
ElliotHume <elliot@hume.ca>
2022-04-01 13:19:57 -0400

    Finishing up new basics tutorial

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Teleporter : MonoBehaviour {
     public Transform anchor;
     public bool isKillingPlane = false;
+    public string NetworkEffectOnTeleport;
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player" && other.gameObject == PlayerManager.LocalPlayerGameObject) {
@@ -12,6 +14,7 @@ public class Teleporter : MonoBehaviour {
             pm.Teleport(anchor);
 
             if (isKillingPlane) pm.TakeDamage(100000f, new ManaDistribution());
+            if (NetworkEffectOnTeleport != "") PhotonNetwork.Instantiate(NetworkEffectOnTeleport, anchor.position, anchor.rotation);
         }
     }
 }
