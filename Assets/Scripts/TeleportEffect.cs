e568fcd8378c5fdb1aeb73bd145c65bf979d67c5
ElliotHume <elliot@hume.ca>
2022-04-13 23:49:18 -0400

    Add Perfect Defense mastery spell

--- Assets/Scripts/TeleportEffect.cs
+++ Assets/Scripts/TeleportEffect.cs
@@ -7,27 +7,39 @@ using UnityEngine.AI;
 public class TeleportEffect : MonoBehaviourPun {
 
     public Vector3 direction = Vector3.zero;
-    public float randomSphereRadius = 0f;
+    public float randomSphereRadius = 0f, delay = 0f;
 
     public bool useNavMesh = true;
-    public bool playerOriented = false;
+    public bool playerOriented = false, isAnchor = false, teleportOwnerOnAwake = false;
     public bool canHitSelf = false, onlyHitSelf;
     public bool singleUse = false;
     public bool isAffectedBySpellStrength = true;
 
     public string NetworkEffectOnTeleport;
+    public bool putEffectAtSource = false;
 
-    private bool isCollided = false;
+    private bool isCollided = false, hasTeleportedOwner = false;
     private Spell attachedSpell;
 
     void Start() {
         attachedSpell = GetComponent<Spell>();
     }
 
+    void FixedUpdate() {
+        if (teleportOwnerOnAwake && !hasTeleportedOwner && attachedSpell != null && attachedSpell.GetOwner() != null) {
+            ManualActivation(attachedSpell.GetOwner());
+            hasTeleportedOwner = true;
+        }
+    }
+
     public void ManualActivation(GameObject playerGO) {
         if (!photonView.IsMine) return;
         if (playerGO != null) {
-            Activate(playerGO);
+            if (delay > 0f) {
+                StartCoroutine(DelayedActivation(playerGO));
+            } else {
+                Activate(playerGO);
+            }
         }
     }
 
@@ -35,7 +47,11 @@ public class TeleportEffect : MonoBehaviourPun {
         Debug.Log("Collided with: "+collision.gameObject);
         if (photonView.IsMine && !isCollided) {
             if (collision.gameObject.tag == "Player" && (collision.gameObject != PlayerManager.LocalPlayerGameObject || canHitSelf) && !(onlyHitSelf && collision.gameObject != PlayerManager.LocalPlayerGameObject)) {
-                Activate(collision.gameObject);
+                if (delay > 0f) {
+                    StartCoroutine(DelayedActivation(collision.gameObject));
+                } else {
+                    Activate(collision.gameObject);
+                }
             }
         }
     }
@@ -43,7 +59,11 @@ public class TeleportEffect : MonoBehaviourPun {
     void OnTriggerEnter(Collider collision) {
         if (photonView.IsMine && !isCollided) {
             if (collision.gameObject.tag == "Player" && (collision.gameObject != PlayerManager.LocalPlayerGameObject || canHitSelf) && !(onlyHitSelf && collision.gameObject != PlayerManager.LocalPlayerGameObject)) {
-                Activate(collision.gameObject);
+                if (delay > 0f) {
+                    StartCoroutine(DelayedActivation(collision.gameObject));
+                } else {
+                    Activate(collision.gameObject);
+                }
             }
         }
     }
@@ -57,7 +77,7 @@ public class TeleportEffect : MonoBehaviourPun {
             float multiplier = attachedSpell != null && isAffectedBySpellStrength ? attachedSpell.GetSpellStrength() : 1f;
 
             Vector3 fixedDirection = direction * multiplier;
-            Vector3 destination = player.transform.position + (randomSphereRadius > 0f ? Random.insideUnitSphere * randomSphereRadius * multiplier :
+            Vector3 destination = (!isAnchor ? player.transform.position : transform.position) + (randomSphereRadius > 0f ? Random.insideUnitSphere * randomSphereRadius * multiplier :
                     (playerOriented 
                         ? (player.transform.forward * fixedDirection.z + player.transform.right * fixedDirection.x + player.transform.up * fixedDirection.y)
                         : (transform.forward * fixedDirection.z + transform.right * fixedDirection.x + transform.up * fixedDirection.y)
@@ -78,18 +98,29 @@ public class TeleportEffect : MonoBehaviourPun {
             RaycastHit rayHit;
             if (!Physics.CheckSphere(destination, 2f, 0, QueryTriggerInteraction.Ignore) && Physics.SphereCast(destination, 2, Vector3.down, out rayHit, Mathf.Max(randomSphereRadius, 25f))){
                 pv.RPC("TeleportEffect", RpcTarget.All, destination);
-                if (NetworkEffectOnTeleport != "") PhotonNetwork.Instantiate(NetworkEffectOnTeleport, destination, Quaternion.identity);
+                if (NetworkEffectOnTeleport != "") {
+                    PhotonNetwork.Instantiate(NetworkEffectOnTeleport, destination, Quaternion.identity);
+                    if (putEffectAtSource) PhotonNetwork.Instantiate(NetworkEffectOnTeleport, player.transform.position, Quaternion.identity);
+                }
                 return;
             } else {
                 NavMeshHit hit;
                 if (NavMesh.SamplePosition(destination, out hit, Mathf.Max(randomSphereRadius, 10f), NavMesh.AllAreas)) {
                     pv.RPC("TeleportEffect", RpcTarget.All, hit.position);
-                    if (NetworkEffectOnTeleport != "") PhotonNetwork.Instantiate(NetworkEffectOnTeleport, hit.position, Quaternion.identity);
+                    if (NetworkEffectOnTeleport != "") {
+                        PhotonNetwork.Instantiate(NetworkEffectOnTeleport, hit.position, Quaternion.identity);
+                        if (putEffectAtSource) PhotonNetwork.Instantiate(NetworkEffectOnTeleport, player.transform.position, Quaternion.identity);
+                    }
                     return;
                 }
             }
             PhotonNetwork.Instantiate("XCollision_Fizzle", player.transform.position, Quaternion.identity);
         }
+    }
+
+    IEnumerator DelayedActivation(GameObject go) {
+        yield return new WaitForSeconds(delay);
+        Activate(go);
     }
 
     void OnDrawGizmosSelected() {
